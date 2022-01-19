a6c10f6008b8d91fc1c2a87f7b38fbae9e03e0d5
ijh4u <ijh4u24@gmail.com>
2022-01-19 16:30:17 +0900

    laws of big numbers

---
+++ main.py
@@ -0,0 +1,10 @@
+n,m,k=map(int,input().split())
+data=list(map(int, input().split()))
+data.sort()
+first=data[n-1]
+second=data[n-2]
+
+count=m//(k+1)*k
+count+=m%(k+1)
+result= (first*count) +(second*(m-count))
+print(result)
